78ee76de3ee50475b8f5c1f3906cbd01f6524db8
Mark Halonen <halonen.mark@gmail.com>
2018-11-15 20:49:19 -0500

    try to fix travis

--- __chester__/tests/4/run_test/command.sh
+++ __chester__/tests/4/run_test/command.sh
@@ -3,7 +3,7 @@ cd workspace
 chester init
 chester create --silent "$(< ../echo_json.txt)"
 chester test --silent
-echo '{"k1":"v2"}' > __chester__/tests/1/expected_output.txt
+echo '{"k1":"v2"}' > ./__chester__/tests/1/expected_output.txt
 chester test --silent
 cd ..
 rm -rf workspace
