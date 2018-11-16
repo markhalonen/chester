60eae09d57abf4ec128ccce4ccd932bb83370990
Mark Halonen <halonen.mark@gmail.com>
2018-11-15 21:00:05 -0500

    another try

--- __chester__/tests/4/run_test/command.sh
+++ __chester__/tests/4/run_test/command.sh
@@ -1,9 +1,21 @@
 mkdir workspace
 cd workspace
 chester init
-chester create --silent "$(< ../echo_json.txt)"
+
+mkdir my_test
+cd my_test
+echo 'import json' > script.py
+echo 'print json.dumps({"k1": "v1"})' >> script.py
+echo 'python script.py' > command.sh
+chmod 777 command.sh
+cd ..
+
+chester create --silent my_test
 chester test --silent
+
+# Change the content on purpose so it fails
 echo '{"k1":"v2"}' > ./__chester__/tests/1/expected_output.txt
+
 chester test --silent
 cd ..
 rm -rf workspace
